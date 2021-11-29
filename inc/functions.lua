82936898dffc6222b0159098a5389e5ef81b5ff3
SNKO85 <94779416+SNKO85@users.noreply.github.com>
2021-11-29 17:01:38 +0300

    Update functions.lua

--- inc/functions.lua
+++ inc/functions.lua
@@ -50,9 +50,6 @@ return UpdateM4Query(msg)
 elseif TextCmdquery == "CmdSudo" then
 if not CheckUserAdmin(msg) then return answerCallbackQuery(data.id_,"ليس لديك صلاحية التحكم , ارجو المعذرة ❤️",true) end
 return UpdateM5Query(msg)
-elseif TextCmdquery == "CmdFiles" then
-if not CheckUserAdmin(msg) then return answerCallbackQuery(data.id_,"ليس لديك صلاحية التحكم , ارجو المعذرة ❤️",true) end
-return UpdateM6Query(msg)
 elseif TextCmdquery == "LoginOut" then
 if not CheckUserAdmin(msg) then return answerCallbackQuery(data.id_,"ليس لديك صلاحية التحكم , ارجو المعذرة ❤️",true) end
 msg.Delete = true
