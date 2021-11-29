e0216608ad16a7e34338f3c88fc369e9fdf9c9fb
SNKO85 <94779416+SNKO85@users.noreply.github.com>
2021-11-29 16:45:47 +0300

    Update functions.lua

--- functions.lua
+++ functions.lua
@@ -139,13 +139,11 @@ end
 
 keyboardCmd = [[{
 "inline_keyboard": [
-[{"text": "𝟏","callback_data": "List1"},{"text": "𝟐","callback_data": "List2"}],
-[{"text": "𝟑","callback_data": "List3"},{"text": "𝟒","callback_data": "List4"}],
-[{"text": "𝟓","callback_data": "CmdSudo"},{"text": "𝟔","callback_data": "CmdFiles"}],
+[{"text": "1","callback_data": "List1"},{"text": "2","callback_data": "List2"}],[{"text": "3","callback_data": "List3"},
+{"text": "الالعاب","callback_data": "List4"}],[{"text": "السورس","callback_data": "CmdSudo"},{"text": "𝟔","callback_data": "CmdFiles"}],
 [{"text": "- اخفاء الامر","callback_data": "LoginOut"}]
 ]}
 ]]
-
 
 keyboardSitting = [[{"inline_keyboard": [
 [{"text": "- اعدادات المجموعة","callback_data": "GroupSitting"},{"text": "- اعدادات الوسائط","callback_data": "MediaSitting"}],
